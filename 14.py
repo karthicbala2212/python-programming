a6432f99a2a7aedd56e98f321a0923331a50562e
karthic2224 <51222677+karthicbala2212@users.noreply.github.com>
2019-06-13 16:12:39 +0530

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,5 +1,5 @@
-ax,bx=map(int,input().split())
-if(ax<bx):
-  for i in range(ax+1,bx,1):
-     if i%2!=0:
-        print(i,end=' ')
+a=int(input())
+b=int(input())
+for i in range(a,b+1):
+  if(i%2!=0):
+    print(i,end=' ')
